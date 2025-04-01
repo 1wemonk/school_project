035fd00b62eb88d2ba267403a8ad3be4dd2967c0
1wemonk <boringgtoreitup@gmail.com>
2025-04-01 22:15:25 +0300

    fix

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ async function ensureWebhookIsSet(bot, webhookUrl) {
 
 if (webhookUrl) {
     bot.telegram.webhookReply = true; // Важно для Vercel
-    ensureWebhookIsSet(bot, webhookUrl);
+    await ensureWebhookIsSet(bot, webhookUrl);
 } else {
     console.error('WEBHOOK_URL is not set in environment variables.');
 }
